d50652c2cdae8e6e5d9328b0c6172524837640f8
JHendry <jadelhendry@gmail.com>
2020-06-12 11:09:23 -0400

    Added comments to get rid of linter

--- NinjaExercise8/Exercise5/exercise5.go
+++ NinjaExercise8/Exercise5/exercise5.go
@@ -12,12 +12,14 @@ type user struct {
 	Sayings []string
 }
 
+//ByAge used to sort by age
 type ByAge []user
 
 func (a ByAge) Len() int           { return len(a) }
 func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
 
+//ByLast used to sort by Last
 type ByLast []user
 
 func (l ByLast) Len() int           { return len(l) }
